0d442fe40492d32e696a43e6bf1c70f17e06c874
Aaron <whilter08@gmail.com>
2021-07-15 14:25:48 +0900

    Apply .gitignore

---
+++ out/production/resources/schema.sql
@@ -0,0 +1,10 @@
+drop table if exists PLAYERS;
+create table PLAYERS
+(
+    id          bigint auto_increment not null,
+    name        varchar(255) not null,
+    back_number int          not null,
+    primary key (id)
+);
+
+insert into PLAYERS (name, back_number) values('aaron', 14);
